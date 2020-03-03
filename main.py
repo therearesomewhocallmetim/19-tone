c3bd401136248338df316c53a6fa219a9d198358
Timofey Danshin <t_danshin@gcore.lu>
2020-03-04 00:09:59 +0300

    Now with chords

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import numpy
 import scipy.signal
+import time
 
 
 import pygame, pygame.sndarray
 
 pygame.init()
 
-def play_for(sample_wave, ms):
+def play(sample_wave, ms):
     """Play the given NumPy array, as a sound, for ms milliseconds."""
+    sample_wave = numpy.resize(sample_wave, (sample_wave.shape[0], 2))
     sound = pygame.sndarray.make_sound(sample_wave)
     sound.play(-1)
     pygame.time.delay(ms)
@@ -23,29 +25,74 @@ def sine_wave(hz, peak, n_samples=sample_rate):
     omega = numpy.pi * 2 / length
     xvalues = numpy.arange(int(length)) * omega
     onecycle = peak * numpy.sin(xvalues)
-    x = numpy.resize(onecycle, (n_samples, 2,)).astype(numpy.int16)
+    x = numpy.resize(onecycle, (n_samples,)).astype(numpy.int16)
     return x
 
-def square_wave(hz, peak, duty_cycle=.5, n_samples=sample_rate):
-    """Compute N samples of a sine wave with given frequency and peak amplitude.
-       Defaults to one second.
-    """
-    t = numpy.linspace(0, 1, 500 * 440/hz, endpoint=False)
-    wave = scipy.signal.square(2 * numpy.pi * 5 * t, duty=duty_cycle)
-    wave = numpy.resize(wave, (n_samples,))
-    return (peak / 2 * wave.astype(numpy.int16))
-
-print('play 1')
-
-# Play A (440Hz) for 1 second as a sine wave:
 
 start_freq = 440
 
-for i in range(0, 19):
-    factor = 2 ** (i / 19)
-    play_for(sine_wave(440 * factor, 4096), 1000)
+
+def note(n):
+    factor = 2 ** (n / 19)
+    return sine_wave(440 * factor, 4096)
 
 
 
-def make_chord(*args):
-    ret =
+# for i in range(0, 19):
+#     play(note, 1000)
+
+
+
+def chord(*args):
+    ret = sum((note(arg) for arg in args))
+    return ret
+
+play(chord(0, 3, 6), 1000)
+time.sleep(0.3)
+play(chord(0, 3, 7), 1000)
+time.sleep(0.3)
+play(chord(0, 4, 11), 1000)
+time.sleep(0.3)
+play(chord(0, 5, 11), 1000)
+time.sleep(0.3)
+play(chord(0, 6, 11), 1000)
+# time.sleep(0.3)
+# play(chord(0, 3, 11), 1000)
+# time.sleep(0.3)
+# play(chord(0, 4, 10), 1000)
+
+exit(0)
+
+from collections import Counter, defaultdict
+
+niceness = defaultdict(list)
+
+for i in range(19):
+    print(f"Playing 0, {i}")
+    play(chord(0, 11, i), 1000)
+    time.sleep(0.3)
+    # evaluation = input('Ha?')
+    # c = Counter(evaluation)
+    # niceness[c['+']].append(i)
+
+for x, y in niceness.items():
+    print(f'{x}, {y}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dude
